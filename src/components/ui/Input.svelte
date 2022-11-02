493803e7af95c24112dc78b1f6e4b01cd464e901
Franklin Shera <fshera96@gmail.com>
2022-11-03 00:26:53 +0300

    rest props

--- src/components/ui/Input.svelte
+++ src/components/ui/Input.svelte
@@ -1,18 +1,14 @@
 <script>
-	// @ts-nocheck
-
 	export let inputLabel = '';
 	export let inputValue = '';
-
-	export let otherInputProps;
 
 	let focused = false;
 </script>
 
-<div class="input-group relative w-full overflow-hidden">
+<div class="input-group relative w-full overflow-hidden font-semibold">
 	<label
 		for=""
-		class="pointer-events-none absolute transition-transform duration-200 top-[50%] -translate-y-[50%] {focused
+		class="pointer-events-none text-gray-100 absolute transition-transform duration-200 top-[50%] -translate-y-[50%] {focused
 			? '-translate-x-[100%]'
 			: ''}">{inputLabel}</label
 	>
@@ -23,7 +19,7 @@
 			focused = false;
 		}}
 		bind:value={inputValue}
-		{...otherInputProps}
-		class="border-b border-[#313638] bg-transparent w-full p-2 outline-none"
+		{...$$restProps}
+		class="border-b border-[#313638] bg-transparent text-gray-100 w-full p-2 outline-none"
 	/>
 </div>
